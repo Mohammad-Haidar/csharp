372a617036d0b903d3943b9ab010675b47171681
Mohammad-Haidar <163796786+Mohammad-Haidar@users.noreply.github.com>
2024-06-08 21:10:56 +0530

    Update SwapNumbers.cs

--- Assignments/SwapNumbers.cs
+++ Assignments/SwapNumbers.cs
@@ -1,4 +1,26 @@
-Console.WriteLine("Enter the value of a  : ");
-int a = Console.ReadLine();
-Console.WriteLine("Enter the value of b  : ");
-int b = Console.ReadLine();
+using System;
+
+class SwapNumbers
+{
+    static void Main()
+    {
+        // Prompt the user to enter the first number
+        Console.Write("Enter the first number: ");
+        int number1 = int.Parse(Console.ReadLine());
+
+        // Prompt the user to enter the second number
+        Console.Write("Enter the second number: ");
+        int number2 = int.Parse(Console.ReadLine());
+
+        // Display the numbers before swapping
+        Console.WriteLine($"\nBefore swapping: number1 = {number1}, number2 = {number2}");
+
+        // Swap the numbers
+        int temp = number1;
+        number1 = number2;
+        number2 = temp;
+
+        // Display the numbers after swapping
+        Console.WriteLine($"After swapping: number1 = {number1}, number2 = {number2}");
+    }
+}
